c4c78f284d4bc0dca147d056e92aaeff65baa13e
VividLiu <touch3247@gmail.com>
2015-12-16 17:18:59 -0800

    add jquery event listeners

--- js/app.js
+++ js/app.js
@@ -10,24 +10,53 @@ $(document).ready(function(){
 		$(".Ryu-ready").hide();
 	})
 
-	// $(".Ryu").click(function(){
+
+	// $(".Ryu").on("mousedown", function(){
+	// 	console.log("mousedown");
 	// 	$(".Ryu-ready").hide();
 	// 	$(".Ryu-throwing").show();
-	// });
-	$(".Ryu").on('click', function(){
+	// }).off("mousedown", function(){
+	// 	console.log("mouseup");
+	// 	$(".Ryu-throwing").hide();
+	// 	$(".Ryu-ready").show();
+	// })
+
+	$(".Ryu").mousedown(function(){
+		console.log("mousedown");
 		$(".Ryu-ready").hide();
 		$(".Ryu-throwing").show();
-	});
+		// $(".hadouken").show();
 
-	$(".Ryu").off("click", function(){
+		//animate hadouken
+		$(".hadouken").finish().show().animate({
+			"margin-left": "64em"
+			// "margin-left": "+= 5"
+		}, 300, function(){
+			//animation complete
+			$(this).hide()
+			$(this).css("margin-left", "32em");
+		});
+
+		//play sound
+		$("#hadouken-sound")[0].volume = 0.5;
+		$("#hadouken-sound")[0].load();
+		$("#hadouken-sound")[0].play();
+	}).mouseup(function(){
+		console.log("mouseup");
 		$(".Ryu-throwing").hide();
 		$(".Ryu-ready").show();
 	})
 
 	$(document).keydown(function(key){
-		if(key.which == 120){
-			alert("done!");
+		if(key.which == 88){
+			$(".Ryu-ready").hide();
+			$(".Ryu-throwing").hide();
+			$(".Ryu-still").hide();
+			$(".Ryu-cool").show();
 		}
+	}).keyup(function(key){
+		$(".Ryu-ready").show();
+		$(".Ryu-cool").hide();
 	})
 
 
