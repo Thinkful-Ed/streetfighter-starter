ca9430b27f03221f347da7580d8cbd0eb7fa8022
tihomir-uzelac <tihomir.uzelac@gmail.com>
2016-01-08 19:28:37 +0000

    ryu mouseenter and mouseleave added

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+	$('.ryu').mouseenter(function() {
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function() {
+   		$('.ryu-ready').hide();
+   		$('.ryu-still').show();
+  });
+});
