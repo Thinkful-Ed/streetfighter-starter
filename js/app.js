4417a0829042197b3f92940714c5de0e4ba25a3c
tihomir-uzelac <tihomir.uzelac@gmail.com>
2016-01-17 17:52:35 +0000

    hadouken trown in

--- js/app.js
+++ js/app.js
@@ -1,11 +1,35 @@
 $(document).ready(function() {
 
-	$('.ryu').mouseenter(function() {
-		$('.ryu-still').hide();
-		$('.ryu-ready').show();
-	})
-	.mouseleave(function() {
-   		$('.ryu-ready').hide();
-   		$('.ryu-still').show();
-  });
+	//Mouse over for ready position
+	$(".ryu").mouseenter(function() {
+		$(".ryu-still").hide();
+		$(".ryu-ready").show();
+	});
+	$(".ryu").mouseleave(function() {
+   		$(".ryu-ready").hide();
+   		$(".ryu-still").show();
+    });
+
+    //On mouse click trow hadouken
+	$(".ryu").mousedown(function() {
+		$(".ryu-ready").hide();
+    	$(".ryu-throwing").show();
+    	$(".hadouken").show();
+    	$(".hadouken").animate({left: "+=530px"}, 400);
+	});
+	$(".ryu").mouseup(function() {
+    	$(".ryu-ready").show();
+    	$(".ryu-throwing").hide();
+    	$(".hadouken").hide();
+    	$(".hadouken").animate({left: "-=530px"}, 0);
+    });
+
+	//When X is pressed go to cool pose
+	$(".ryu").keydown(function() {
+			$(".ryu-still").hide();
+			$(".ryu-cool").show();
+	});
+
+
+
 });
