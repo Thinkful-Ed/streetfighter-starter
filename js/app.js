43addcdce4b21f4633e2d2d5379289eacd1b15cd
tihomir-uzelac <tihomir.uzelac@gmail.com>
2016-01-19 21:52:55 +0000

    sound on kewdown added, project completed

--- js/app.js
+++ js/app.js
@@ -1,35 +1,58 @@
 $(document).ready(function() {
+	//Variable declaration
+	var ryu = $("#ryu"),
+		ryuStill = $("#ryu-still"),
+		ryuReady = $("#ryu-ready"),
+		ryuThrowing = $("#ryu-throwing"),
+		ryuHadouken = $("#hadouken"),
+		ryuCool = $("#ryu-cool"),
+		hadoukenSound = $("#hadouken-sound")[0],
+		coolSound = $("#cool-sound")[0];
+
 
 	//Mouse over for ready position
-	$(".ryu").mouseenter(function() {
-		$(".ryu-still").hide();
-		$(".ryu-ready").show();
+	ryu.mouseenter(function() {
+		ryuStill.hide();
+		ryuReady.show();
 	});
-	$(".ryu").mouseleave(function() {
-   		$(".ryu-ready").hide();
-   		$(".ryu-still").show();
+	ryu.mouseleave(function() {
+   		ryuReady.hide();
+   		ryuStill.show();
     });
 
     //On mouse click trow hadouken
-	$(".ryu").mousedown(function() {
-		$(".ryu-ready").hide();
-    	$(".ryu-throwing").show();
-    	$(".hadouken").show();
-    	$(".hadouken").animate({left: "+=530px"}, 400);
+	ryu.mousedown(function() {
+		ryuReady.hide();
+    	ryuThrowing.show();
+    	ryuHadouken.show().animate({left: "+=530px"}, 400).fadeOut("fast");
+    	hadoukenSound.play();
 	});
-	$(".ryu").mouseup(function() {
-    	$(".ryu-ready").show();
-    	$(".ryu-throwing").hide();
-    	$(".hadouken").hide();
-    	$(".hadouken").animate({left: "-=530px"}, 0);
+	ryu.mouseup(function() {
+    	ryuReady.show();
+    	ryuThrowing.hide();
+    	ryuHadouken.hide().animate({left: "-=530px"}, 0);
     });
 
-	//When X is pressed go to cool pose
-	$(".ryu").keydown(function() {
-			$(".ryu-still").hide();
-			$(".ryu-cool").show();
-	});
 
+	//When X is pressed go to cool pose
+	$(document).keydown(function(e) {
+		if(e.which == 88) {
+			ryuStill.hide();
+			ryuReady.hide();
+			ryuHadouken.hide();
+			ryuCool.show();
+			coolSound.play();
+		}
+	});
+	$(document).keyup(function(e) {
+		if(e.which == 88) {
+			ryuStill.show();
+			ryuReady.hide();
+			ryuHadouken.hide();
+			ryuCool.hide();
+			coolSound.pause();
+		}
+	});
 
 
 });
