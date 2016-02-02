7a391c186571fbf8a31bd073f7ed7e1a1a1bf0ba
John Coppola <jdcoppola@gmail.com>
2016-02-02 23:29:06 +0100

    ryu ready to fight

--- js/app.js
+++ js/app.js
@@ -8,14 +8,26 @@ $(document).ready(function(){
     $('ryu.ready').hide();
   })
   .mousedown(function(){
-    // play hadouken sound
+    playHadouken();
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
-    $('.hadouken').show();
-    // show hadouken and animate it moving to the right
+    $('.hadouken').finish().show().animate(
+      {'left': '1020px'},
+      500,
+      function(){
+        $(this).hide();
+        $(this).css('left', '520px');
+      }
+    );
   })
   .mouseup(function(){
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
   });
 });
+
+function playHadouken() {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
