ce1489896559d7e43fa8f63eb220449b86d97878
John Coppola <jdcoppola@gmail.com>
2016-02-02 23:08:12 +0100

    ryu finished

--- js/app.js
+++ js/app.js
@@ -6,5 +6,16 @@ $(document).ready(function(){
   .mouseleave(function(){
     $('.ryu-still').show();
     $('ryu.ready').hide();
+  })
+  .mousedown(function(){
+    // play hadouken sound
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').show();
+    // show hadouken and animate it moving to the right
+  })
+  .mouseup(function(){
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
   });
 });
