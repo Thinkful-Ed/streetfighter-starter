c55e95fe88be2fa68053b2c8b66cfb39bc9eeca8
John Coppola <jdcoppola@gmail.com>
2016-02-02 22:56:11 +0100

    first events

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.ryu').mouseenter(function(){
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function(){
+    $('.ryu-still').show();
+    $('ryu.ready').hide();
+  });
+});
