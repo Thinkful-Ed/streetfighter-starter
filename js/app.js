b1cfcfb3a8fe4d52c338abc867ff4a3feee8b8a4
green6erry <green6erry@gmail.com>
2016-03-03 13:26:28 -0500

    Mo Finished
    Added keyup/down thing

--- js/app.js
+++ js/app.js
@@ -25,10 +25,26 @@ $(document).ready(function(){
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
+	})
 	});
 
+	$(document)
+	.keydown(function(e) {
+	    if (e.keyCode == 88) {
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-cool').show();
+			}
+		})
+	.keyup(function(e) {
+	    if (e.keyCode == 88) {
+			$('.ryu-cool').hide();
+			$('.ryu-still').hide();
+			$('.ryu-ready').show();
+			}
+		}
+		);
 
-});
 
 function playHadouken() {
 	$('#hadouken-sound')[0].volume = 0.5;
