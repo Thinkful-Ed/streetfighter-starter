b3410d85637fc313be41434a7c2474e4aa02f116
green6erry <green6erry@gmail.com>
2016-03-03 11:42:24 -0500

    Finished
    Added sound, animation, etc.

---
+++ js/app.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+	$('.ryu').mouseenter(function() {
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	.mousedown(function() {
+		playHadouken();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show()
+		.animate(
+			{'left': '1200px'},
+			1000,
+			function () {
+			$(this).hide();
+			$(this).css('left', '520px');
+			}
+		);
+	})
+	.mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	});
+
+
+});
+
+function playHadouken() {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
