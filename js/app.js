10fb1ffe478cc6589841058d26a17e4aac518a4e
Dan Riding <doonyby@gmail.com>
2016-03-14 13:50:08 -0600

    animate hadouken

--- js/app.js
+++ js/app.js
@@ -3,14 +3,35 @@ $(document).ready(function() {
     $('.ryu-still').hide();
     $('.ryu-ready').show();
   })
+
   .mouseleave(function() {
     $('.ryu-ready').hide();
  	$('.ryu-still').show();
+  })
+
+  .mousedown(function() {
+    console.log('mousedown');
+    // play hadouken sound
+    $('.ryu-ready').hide();
+  	$('.ryu-throwing').show();
+	$('.hadouken').finish().show().animate(
+  		{'left': '1020px'},
+  		500,
+  		function() {
+    		$(this).hide();
+    		$(this).css('left', '520px');
+  		}
+	);
+  	})
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+  	$('.ryu-ready').show();
+
+
+
+
+
   });
-
-
-
-
 });
 
 
