dd75dda6d3d904bf33f67054fbdd396b30753375
Dan Riding <doonyby@gmail.com>
2016-03-14 12:39:57 -0600

    ryu ready and still

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
   $('.ryu').mouseenter(function() {
-    alert('mouse entered .ryu div');
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+ 	$('.ryu-still').show();
   });
+
+
+
+
 });
+
+
