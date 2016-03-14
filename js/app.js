ce7320ee8649d644b82b53c965ad6acf6c851b67
Dan Riding <doonyby@gmail.com>
2016-03-14 14:22:02 -0600

    finished sound

--- js/app.js
+++ js/app.js
@@ -10,8 +10,7 @@ $(document).ready(function() {
   })
 
   .mousedown(function() {
-    console.log('mousedown');
-    // play hadouken sound
+  	playHadouken();
     $('.ryu-ready').hide();
   	$('.ryu-throwing').show();
 	$('.hadouken').finish().show().animate(
@@ -20,18 +19,17 @@ $(document).ready(function() {
   		function() {
     		$(this).hide();
     		$(this).css('left', '520px');
-  		}
-	);
+  		});
   	})
   .mouseup(function() {
     $('.ryu-throwing').hide();
   	$('.ryu-ready').show();
-
-
-
-
-
   });
 });
 
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
 
