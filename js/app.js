fb0251db278434f429d4991cbbbf802b333d53d3
Dustin Henrich <dustin.henrich@gmail.com>
2016-04-10 11:42:34 -0500

    Added Ryu animations
    Made Ryu animate when mouse enter, mouse click, and mouse release.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
-(document).ready(function() {
+$(document).ready(function() {
   $('.ryu').mouseenter(function() {
-    alert('mouse entered .ryu div');
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+  	$('.ryu-ready').hide();
+  	$('.ryu-still').show();
+  })
+  .mousedown(function() {
+  	// play hadouken sound
+  	$('.ryu-ready').hide();
+  	$('.ryu-throwing').show();
+  	$('hadouken').show();
+    // show hadouken and animate it to the right of the screen
+  })
+  .mouseup(function(){
+  	// ryu goes back to his ready position
+  	$('.ryu-throwing').hide();
+  	$('.ryu-ready').show();
   });
 });
