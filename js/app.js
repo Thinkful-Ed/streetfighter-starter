02507239f720eeb49ead5c05d8aea788fcdb8fb0
Dustin Henrich <dustin.henrich@gmail.com>
2016-04-10 12:41:41 -0500

    Final touches
    Finished animation and added sound to animation

--- js/app.js
+++ js/app.js
@@ -8,15 +8,26 @@ $(document).ready(function() {
   	$('.ryu-still').show();
   })
   .mousedown(function() {
-  	// play hadouken sound
+  	playHadouken();
   	$('.ryu-ready').hide();
   	$('.ryu-throwing').show();
-  	$('hadouken').show();
-    // show hadouken and animate it to the right of the screen
+  	$('.hadouken').finish().show().animate(
+  		{'left':'1020px'},
+  		500,
+  		function() {
+			$(this).hide();
+			$(this).css('left','555px');
+  		});
   })
   .mouseup(function(){
-  	// ryu goes back to his ready position
   	$('.ryu-throwing').hide();
   	$('.ryu-ready').show();
   });
+
 });
+
+function playHadouken() {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
