720df848586121e527536c09a774b3c18d33e9fc
scottdiallo <diallot@me.com>
2016-04-11 13:12:42 -0700

    hadouken sound added

--- js/app.js
+++ js/app.js
@@ -8,10 +8,15 @@ $(document).ready(function () {
             $('.ryu-still').show();
         })
         .mousedown(function () {
-            // play hadouken sound
+            playHadouken();
             $('.ryu-ready').hide();
             $('.ryu-throwing').show();
-            $('.hadouken').show();
+            $('.hadouken').finish().show().animate({'left':'1020px'},
+                500,
+                function() {
+                    $(this).hide();
+                    $(this).css('left','520px');
+                });
             // show hadouken and animate to right of screen
         })
         .mouseup(function () {
@@ -19,6 +24,13 @@ $(document).ready(function () {
             $('.ryu-ready').show();
             // ryu goes back to his ready position
         });
+
+        
 });
 
+function playHadouken(){
+    $ ('#hadouken-sound')[0].volume = 0.5;
+    $ ('#hadouken-sound')[0].load();
+    $ ('#hadouken-sound')[0].play();
+};
 //Scott was here!
