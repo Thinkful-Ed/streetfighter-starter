e6f71062b0fc278a3fe1e437c61ede081275ae8a
Kyle <kyleludlow@gmail.com>
2016-04-11 12:15:20 -0700

    Added jquery for showing ready, still, and throwing positions and mouse click function

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $('.ryu').mouseenter(function () {
+            $('.ryu-still').hide();
+            $('.ryu-ready').show();
+        })
+        .mouseleave(function () {
+            $('.ryu-ready').hide();
+            $('.ryu-still').show();
+        })
+        .mousedown(function () {
+            // play hadouken sound
+            $('.ryu-ready').hide();
+            $('.ryu-throwing').show();
+            $('.hadouken').show();
+            // show hadouken and animate to right of screen
+        })
+        .mouseup(function () {
+            $('.ryu-throwing').hide();
+            $('.ryu-ready').show();
+            // ryu goes back to his ready position
+        });
+});
