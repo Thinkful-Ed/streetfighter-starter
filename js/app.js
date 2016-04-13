ba4f28c5c2a3011c5e9fc3bf0d41841e527735eb
Travis Rose <travis@carrythebanner.com>
2016-04-12 23:46:30 -0700

    Problems with line 17-22

--- js/app.js
+++ js/app.js
@@ -1,26 +1,30 @@
 $(document).ready(function(){
 
-	$('.ryu').mouseenter(function(){
-		$('.ryu-still').hide();
-		$('.ryu-ready').show();
-	});
+   $('.ryu').mouseenter(function(){
+       $('.ryu-still').hide();
+       $('.ryu-ready').show();
+   });
 
-	$('.ryu').mouseleave(function(){
-		$('.ryu-still').show();
-		$('.ryu-ready').hide();
-	});
+   $('.ryu').mouseleave(function(){
+       $('.ryu-still').show();
+       $('.ryu-ready').hide();
+   });
 
-	$('.ryu').mousedown(function(){
-		$('.ryu-ready').hide();
-		$('.ryu-throwing').show();
-		$('.hadouken').css({"opacity" : "1"});
-	});
+   $('.ryu').mousedown(function(){
+       $('.ryu-ready').hide();
+       $('.ryu-throwing').show();
+       $('.hadouken').css({"opacity" : "1"})
+           .animate({"z-index" : "9"},
+               step: function(now,fx) {
+                   $(this).css('-webkit-transform','translate(100px,0px)')},
+                   duration: 'slow'},'linear');
+               });
+   });
 
-	$('.ryu').mouseup(function(){
-		$('.ryu-ready').show();
-		$('.ryu-throwing').hide();
-		$('.hadouken').css({"opacity" : "0"});
+   $('.ryu').mouseup(function(){
+       $('.ryu-ready').show();
+       $('.ryu-throwing').hide();
+       $('.hadouken').css({"opacity" : "0"});
+   });
 
-	});
-
-});
+});`
