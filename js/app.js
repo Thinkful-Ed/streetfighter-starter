04c0308cb6e5675011e77307ac0a4ae26536b9b4
Ryan Johnson <ryj1023@yahoo.com>
2016-04-25 19:26:57 -0400

    second commit

--- js/app.js
+++ js/app.js
@@ -4,21 +4,45 @@ $('.ryu').mouseenter(function(){
 	$('.ryu-ready').show();
 	})
 .mouseleave(function(){
-	$('.ryu-steady').hide();
 	$('.ryu-still').show();
+    $('.ryu-ready').hide();
 })
   .mousedown(function() {
-  	// play hadouken sound
+  	playHadouken();
   	$('.ryu-ready').hide();
   	$('.ryu-throwing').show();
-  	$('.hadouken').show();
+  	$('.hadouken').finish().show().animate(
+  		{'left': '1020px'}, 500,
+  		function(){
+  			$(this).hide();
+  			$(this).css('left','600px');
+  		
+  		});
+
+  	})
   	// show hadouken and animate it to the right of the screen
-    console.log('mousedown');
-    
-    
-  })
   .mouseup(function() {
-    console.log('mouseup');
+    $('ryu-throwing').hide();
+    $('.ryu-ready').show();
     // ryu goes back to his ready position
-  });
 });
+ $(document).keydown(function(e){
+	if (e.keyCode == 88){
+	$('.ryu-ready').hide();
+	$('.ryu-throwing').hide();
+	$('.ryu-cool').show();
+	}
+ }).keyup(function(e){
+if (e.keyCode == 88){
+	$('.ryu-cool').hide();
+	$('.ryu-ready').show();
+	}
+})
+
+  });
+
+ function playHadouken (){
+  	$('#hadouken-sound')[0].volume = 0.5;
+  	$('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
