658d47d3b8fd69da77d1a612069a6d68931044f3
Amixer <aealbaugh@soloprowebsites.com>
2016-05-10 00:32:23 -0400

    Finished!

--- js/app.js
+++ js/app.js
@@ -2,15 +2,18 @@ $(document).ready(function() {
 	$('.ryu').mouseenter(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
+		$('ryu-cool').hide()
 	})
 	.mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
+		$('.ryu-cool').hide();
 
 	})
 	.mousedown(function() {
 		playHadouken();
 		$('.ryu-ready').hide();
+		$('ryu-cool').hide()
 		$('.ryu-throwing').show();
 		$('.hadouken').finish().show()
 		.animate(
@@ -21,14 +24,25 @@ $(document).ready(function() {
 				$(this).css('left', '520px');
 			}
 			);
-		
-
-		
-	})
+		})
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
+	
+	$(document).on('keydown', function (e) {
+			var code = e.keyCode;
+			if (code == 88) {
+			$('.ryu-ready').hide();
+			$('.ryu-cool').show();
+		}});
+	$(document).on('keyup', function (e){
+		var code = e.keyCode;
+		if (code == 88) {
+			$('.ryu-ready').show();
+			$('.ryu-cool').hide();
+		}
+	})
 });
 
 function playHadouken () {
