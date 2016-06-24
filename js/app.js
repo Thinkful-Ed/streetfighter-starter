36bf84ded371732c6ec4bdf3bf87289c95d331d9
Mostafa <m.elsheikh11791@gmail.com>
2016-06-24 16:59:48 +0200

    my new feature

--- js/app.js
+++ js/app.js
@@ -24,6 +24,9 @@ $(document).ready(function(){
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	})
+
+/*Implemented new feature*/
+
 $(document).keydown(function(event) {
 	if (event.which == 88) {
 		$('.ryu-still').hide();
@@ -31,7 +34,16 @@ $(document).keydown(function(event) {
 		$('.ryu-cool').show();
 	}
 	});
+
+	$(document).keyup(function(event) {
+		if (event.which == 88) {
+			$('.ryu-still').show();
+			$('.ryu-cool').hide();
+		}
+	});
+
 });
+
 function playHadouken () {
 	$('#hadouken-sound')[0].volume = 0.5;
 	$("#hadouken-sound")[0].load();
