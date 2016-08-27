3d24f9172ed75ad650f0c6456520ca7d76a3f2e0
victorsc45 <victorlitzau@gmail.com>
2016-08-27 14:07:34 -0400

    trowning sound and x key cool stance done

--- js/app.js
+++ js/app.js
@@ -1,44 +1,55 @@
 $(document).ready(function(){
-		$('.ryu').mouseenter(function() {
-		$('.ryu-still').hide();
-		$('.ryu-ready').show();
-	})
-
-	.mouseleave(function() {
-		$('.ryu-ready').hide();
-		$('.ryu-still').show();
-	})
-	.mousedown(function() {
-		playHadouken();
-		$('.ryu-ready').hide();
+    $('.ryu').mouseenter(function(){
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+    })
+    .mouseleave(function() {
+        $('.ryu-still').show();
+        $('.ryu-ready').hide();
+    })
+    .mousedown(function() {
+  		playHadouken();
+  		$('.ryu-ready').hide();
   		$('.ryu-throwing').show();
-  		$('.hadouken').show().animate(
-  {'left': '1020px'},
-  500,
-  function() {
-    $('.hadouken').hide();
-    $('.hadouken').css('left', '520px');
-  });
+  		$('.hadouken').show()
+  		.animate(
+    	{'left': '1020px'}, 500,
+    function() {
+      	$('.hadouken').hide();
+      	$('.hadouken').css('left', '520px');
+    }); 
 
- 		
-  	})
-  
-	.mouseup(function() {
-    	$('.ryu-throwing').hide();
-    	$('.ryu-ready').show();
 	})
-	.keydown(function ( event ) {
+    .mouseup(function(){
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    });
+ 	
+
+});
+
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
+//press "x" key to go into cool stance
+$(document).keydown(function ( event ) {
       if (event.which == 88) {
             $('.ryu-cool').show();
+            $('.ryu-ready').hide();
             $('.ryu-still').hide();
         }
-      });
-	
-});
-function playHadouken () {
-  $('#hadouken-sound')[0].volume = 0.5;
-  $('#hadouken-sound')[0].load();
-  $('#hadouken-sound')[0].play();
-}
+    })
+//press any key to have ryu at the ready
+$(document).keyup(function (event) {
+	if (event.which != 88) {
+		$('.ryu-ready').show();
+	}
+})
+
+
+
+
 
 
