85b146e814efe2d3d3627825b59d95ae140861bc
chasingSublimity <blakemsager@gmail.com>
2016-10-16 21:33:33 -0500

    add hadouken sound

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
 		$(".ryu-still").show();
 	})
 	.mousedown(function() {
+		playHadouken();
 		$(".ryu-ready").hide();
 		$(".ryu-throwing").show();
 		$(".hadouken").finish().show().animate(
@@ -18,8 +19,6 @@ $(document).ready(function() {
 				$(".hadouken").css('left', '520px');
 			}
 		);
-		//play hadouken sound
-		//show hadouken slide
 	})
 	.mouseup(function() { 
 		console.log('mouseup');
@@ -28,3 +27,9 @@ $(document).ready(function() {
 
 	});
 });
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
